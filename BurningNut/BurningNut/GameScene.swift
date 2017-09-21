1516ba7c26919ece6d56d290dadfe51e6289d41e
vi13 <victor-gerling@online.de>
2017-09-21 15:39:41 +0200

    collision

--- BurningNut/BurningNut/GameScene.swift
+++ BurningNut/BurningNut/GameScene.swift
@@ -12,8 +12,8 @@ import GameplayKit
 struct PhysicsCategory {
     static let None      : UInt32 = 0
     static let All       : UInt32 = UInt32.max
-    static let Squirrel   : UInt32 = 1
-    static let Bomb: UInt32 = 2
+    static let Squirrel   : UInt32 = 0b1
+    static let Bomb: UInt32 = 0b10
 }
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
@@ -29,26 +29,53 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         self.setupMatchfield()
         self.addBomb()
         
-         self.physicsWorld.gravity = CGVector.init(dx: 0.0, dy: -6)
+        self.physicsWorld.gravity = CGVector.init(dx: 0.0, dy: -6)
+        physicsWorld.contactDelegate = self
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
         
-       
+        var firstBody: SKPhysicsBody
+        var secondBody: SKPhysicsBody
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+        } else {
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+        }
+
+        if ((firstBody.categoryBitMask & PhysicsCategory.Squirrel != 0) &&
+            (secondBody.categoryBitMask & PhysicsCategory.Bomb != 0)) {
+            if let monster = firstBody.node as? SKSpriteNode, let
+                projectile = secondBody.node as? SKSpriteNode {
+                projectileDidCollideWithMonster(bomb: projectile, squirrel: monster)
+            }
+        }
+    }
+    
+    func projectileDidCollideWithMonster(bomb: SKSpriteNode, squirrel: SKSpriteNode) {
+        print("Hit")
+        bomb.removeFromParent()
+        squirrel.removeFromParent()
     }
     
     func addBomb() {
         
         let bomb = SKSpriteNode(imageNamed: "bomb")
-        bomb.position = CGPoint(x: leftSquirrel.position.x, y: leftSquirrel.position.y)
+        bomb.position = CGPoint(x: leftSquirrel.position.x, y: 0)
         bomb.size = CGSize(width: 15, height: 30)
         bomb.zPosition = 1
         
         bomb.physicsBody = SKPhysicsBody(rectangleOf: bomb.size)
+        rightSquirrel.physicsBody?.isDynamic = false
         bomb.physicsBody?.categoryBitMask = PhysicsCategory.Bomb
         bomb.physicsBody?.contactTestBitMask = PhysicsCategory.Squirrel
         bomb.physicsBody?.collisionBitMask = PhysicsCategory.None
 
         addChild(bomb)
         
-        let moveBomb = SKAction.move(to: CGPoint(x: rightSquirrel.position.x, y: rightSquirrel.position.y), duration: 8)
+        let moveBomb = SKAction.move(to: CGPoint(x: 320, y: 0), duration: 8)
         let rotateBomb = SKAction.rotate(byAngle: -20, duration: 8)
         let groupBomb = SKAction.group([moveBomb, rotateBomb])
         bomb.run(SKAction.repeatForever(groupBomb))
@@ -67,17 +94,17 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         leftSquirrel.zPosition = 1
         addChild(leftSquirrel)
         
-        rightSquirrel.position = CGPoint(x: 280.839, y: -89.305)
+        rightSquirrel.position = CGPoint(x: 307.839, y: -89.305)
         rightSquirrel.size = CGSize(width: 51.321, height: 59.464)
         rightSquirrel.anchorPoint.x = 0.5
         rightSquirrel.anchorPoint.y = 0.5
         rightSquirrel.zPosition = 1
         rightSquirrel.physicsBody = SKPhysicsBody(rectangleOf: rightSquirrel.size)
-        rightSquirrel.physicsBody?.isDynamic = false
+        rightSquirrel.physicsBody?.isDynamic = true
         rightSquirrel.physicsBody?.categoryBitMask = PhysicsCategory.Squirrel
         rightSquirrel.physicsBody?.contactTestBitMask = PhysicsCategory.Bomb
         rightSquirrel.physicsBody?.collisionBitMask = PhysicsCategory.None
-        rightSquirrel.physicsBody?.affectedByGravity = false
+        rightSquirrel.physicsBody?.usesPreciseCollisionDetection = true
         addChild(rightSquirrel)
         
         middleTree.position = CGPoint(x: 0, y: -4.238)
